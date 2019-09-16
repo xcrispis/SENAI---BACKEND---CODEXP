455bdf007e396a86a2cd20c6938ad5e87a9e1e23
Marco F. Basile <uyguy66@hotmail.com>
2019-09-16 12:26:45 -0300

    estudo de variaveis

---
+++ background-CSHARP/Aula 2-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula_2_2 {
+    class Program {
+        static void Main (string[] args) {
+            float n1, n2, n3, media;
+            string resultado;
+
+            Console.WriteLine ("Digite a primeira nota: ");
+            n1 = float.Parse (Console.ReadLine ());
+
+            Console.WriteLine ("Digite a primeira nota: ");
+            n2 = float.Parse (Console.ReadLine ());
+
+            Console.WriteLine ("Digite a primeira nota: ");
+            n3 = float.Parse (Console.ReadLine ());
+
+            media = (n1 + n2 + n3) / 3;
+
+            if (media >= 6) {
+                resultado = "Aprovado";
+            } else if (media < 6 && media >= 4) {
+                resultado = "Recuperaçao";
+            } else {
+                resultado = "reprovado";
+            }
+
+            Console.WriteLine ($"Média : {media} = Aluno está {resultado}");
+        }
+    }
+}
